97e2d44cb7e071d41ff533f34237b5dcc16ded3d
vanilla-wave <vanilla-wave@yandex-team.ru>
2023-07-23 23:55:56 +0200

    feat: better type inference
    Release-As: 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import {getHooks} from './hooks';
 import {Controller} from './controller';
-import type {InitOptions, Preset, PresetStep} from './types';
+import type {
+    InitOptions,
+    Preset,
+    PresetStep,
+    InferStepsFromPreset,
+    InferHintParamsFromPreset,
+    InferHintParamsFromConfig,
+    InferStepsFromConfig,
+    InferPresetsFromConfig,
+} from './types';
 
 let closeHintRef: () => void | undefined;
 let passStepRef: (step: string) => Promise<void>;
@@ -17,20 +26,23 @@ export function passStep(step: string) {
     }
 }
 
-export function createStep<StepSlug extends string, HintParams>(
-    step: PresetStep<StepSlug, HintParams>,
-) {
+export function createStep<
+    StepSlug extends string,
+    HintParams extends Partial<HintParams> = undefined,
+>(step: PresetStep<StepSlug, HintParams>) {
     return step;
 }
 
-export function createPreset<HintParams, Steps extends string>(preset: Preset<HintParams, Steps>) {
-    return preset;
+export function createPreset<T>(preset: T) {
+    return preset as unknown as Preset<InferHintParamsFromPreset<T>, InferStepsFromPreset<T>>;
 }
 
-export function createOnboarding<HintParams, Presets extends string, Steps extends string>(
-    options: InitOptions<HintParams, Presets, Steps>,
-) {
-    const controller = new Controller<HintParams, Presets, Steps>(options);
+export function createOnboarding<T extends InitOptions<any, any, any>>(options: T) {
+    const controller = new Controller<
+        InferHintParamsFromConfig<T>,
+        InferPresetsFromConfig<T>,
+        InferStepsFromConfig<T>
+    >(options);
 
     closeHintRef = controller.closeHint;
     // @ts-ignore
