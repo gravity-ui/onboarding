d08025aa1d1c5784c63ba0fedb98e08e44180a53
vanilla-wave <vanilla-wave@yandex-team.ru>
2023-10-05 23:18:13 +0200

    fix: public ensureRunning method

--- src/controller.ts
+++ src/controller.ts
@@ -433,6 +433,31 @@ export class Controller<HintParams, Presets extends string, Steps extends string
         this.logger.debug('Progress reset finished', presetArg);
     };
 
+    async ensureRunning() {
+        if (this.status === 'active') {
+            return;
+        }
+
+        if (!this.progressLoadingPromise) {
+            this.progressLoadingPromise = this.options.getProgressState();
+        }
+
+        this.logger.debug('Loading onboarding progress data');
+        try {
+            const newProgressState = await this.progressLoadingPromise;
+            this.state.progress = {
+                ...defaultProgress,
+                ...newProgressState,
+            };
+            this.status = 'active';
+            this.emitChange();
+
+            this.logger.debug('Onboarding progress data loaded');
+        } catch (e) {
+            this.logger.error('progress data loading error');
+        }
+    }
+
     private resolvePresetSlug = (presetSlug: Presets) => {
         const preset = this.options.config.presets[presetSlug];
 
@@ -601,31 +626,6 @@ export class Controller<HintParams, Presets extends string, Steps extends string
         this.emitChange();
 
         await this.saveBaseState();
-    }
-
-    private async ensureRunning() {
-        if (this.status === 'active') {
-            return;
-        }
-
-        if (!this.progressLoadingPromise) {
-            this.progressLoadingPromise = this.options.getProgressState();
-        }
-
-        this.logger.debug('Loading onboarding progress data');
-        try {
-            const newProgressState = await this.progressLoadingPromise;
-            this.state.progress = {
-                ...defaultProgress,
-                ...newProgressState,
-            };
-            this.status = 'active';
-            this.emitChange();
-
-            this.logger.debug('Onboarding progress data loaded');
-        } catch (e) {
-            this.logger.error('progress data loading error');
-        }
     }
 
     private ensurePresetExists(preset: string): asserts preset is Presets {
