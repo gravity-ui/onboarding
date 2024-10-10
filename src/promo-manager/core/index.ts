3b0f513a62958e0f0ea1fff27ed9ba7ddb4f6e64
vanilla-wave <vanilla-wave@yandex-team.ru>
2024-10-10 11:34:07 +0200

    fix(promo-managed): allow to enable debug in config

--- src/promo-manager/core/index.ts
+++ src/promo-manager/core/index.ts
@@ -2,18 +2,19 @@ import {Controller} from './controller';
 import {getHooks} from './getHooks';
 import type {PromoOptions} from './types';
 
-const getDebug = () => {
+const getDebug = (options: PromoOptions) => {
     if (typeof window === 'undefined') {
         return false;
     }
 
-    const isDebugModeEnabled = Boolean(window.localStorage.getItem('debugPromoManager'));
+    const isDebugEnabledInLs = Boolean(window.localStorage.getItem('debugPromoManager'));
+    const isDebugEnabledInConfig = Boolean(options.debugMode);
 
-    return isDebugModeEnabled;
+    return isDebugEnabledInConfig || isDebugEnabledInLs;
 };
 
 export function createPromoManager(options: PromoOptions) {
-    const controller = new Controller({...options, debugMode: getDebug()});
+    const controller = new Controller({...options, debugMode: getDebug(options)});
 
     const {usePromoManager, useActivePromo, usePromo} = getHooks(controller);
 
