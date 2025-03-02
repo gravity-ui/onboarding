ff4796e7a882acf74f7491ceaf993533906ecf1d
vanilla-wave <vanilla-wave@yandex-team.ru>
2025-03-02 23:05:17 +0100

    fix(onboarding): wrong step take for shared steps

--- src/tests/steps.test.ts
+++ src/tests/steps.test.ts
@@ -170,30 +170,6 @@ describe('pass step', function () {
         });
     });
 
-    describe('shared steps', function () {
-        const sharedStep = {
-            slug: 'sharedStep',
-            name: '',
-            description: '',
-        };
-
-        it('pass shared step -> pick active preset', async function () {
-            const options = getOptions({
-                availablePresets: ['createProject', 'createQueue'],
-                activePresets: ['createQueue'],
-            });
-            options.config.presets.createProject.steps = [sharedStep];
-            options.config.presets.createQueue.steps = [sharedStep];
-
-            const controller = new Controller(options);
-            await controller.passStep('sharedStep');
-
-            const newState = options.onSave.progress.mock.calls[0][0];
-
-            expect(newState.presetPassedSteps.createQueue).toEqual(['sharedStep']);
-        });
-    });
-
     describe('step hooks', function () {
         describe('onStepPass', () => {
             it('pass step -> call onPass hook', async function () {
